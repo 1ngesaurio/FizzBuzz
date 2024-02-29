5e7c2ee7e2d5a1e4a1b69a8838bdd1c3f20fb4d7
Erickson Tacam <erickson312@gmail.com>
2024-02-28 19:08:48 -0600

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i;//Definimos a "i" como entero
+
+        for(i=1; i<=100;i++) //generamos un ciclo for de 1 a 100
+        {
+            if (i % 3 == 0 & i % 5 == 0)// Analisamos si el numero es divisible entre 3 y 5
+            {
+                Console.WriteLine("FizzBuzz");// imprimimos FizzBuzz si el numero es divisible entre 3 y 5
+
+            } else if (i % 5 == 0)// analisamos si el numero es divsible entre 5 
+            {
+                Console.WriteLine("Buzz");// escribimos Buzz si es divisible entre 5
+            }else if (i % 3 == 0) // analisamos si es divisible entre 3
+            {
+                Console.WriteLine("Fizz");// escribimos Fizz si es divisible entre 3
+            }else
+            {
+                Console.WriteLine(i);// de lo contrario se escribe el numero 
+            }
+        }
+
+    }
+}
